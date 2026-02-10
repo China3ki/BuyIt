380a39fe71a3f2c972ae2a11819bd27edd44f904
China3ki <limon4007@gmail.com>
2026-02-10 20:55:30 +0100

    Add new promo graphics and product cart design

---
+++ BuyIt.Client/Components/HomeSlider.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace BuyIt.Client.Components
+{
+    public partial class HomeSlider
+    {
+        private readonly string[] _promoImagesUrl = ["/images/promo1.png", "/images/promo2.png", "/images/promo3.png", "/images/promo4.png", "/images/promo5.png"];
+        private int _currentImageNumber = 0;
+        /// <summary>
+        /// Sets the currently selected image by specifying its number.
+        /// </summary>
+        /// <param name="imageNumber">The number of the image to select. Must be a valid index corresponding to an available image.</param>
+        private void SelectImage(int imageNumber) => _currentImageNumber = imageNumber;
+    }
+}
