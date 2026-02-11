6f91f47ed17ab0450f53e6dd91b8ab8ebdde1de6
China3ki <limon4007@gmail.com>
2026-02-11 20:24:12 +0100

    Add categories section

---
+++ BuyIt.Client/Components/ProductCart.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BuyIt.Client.Components
+{
+    public partial class ProductCart
+    {
+        [Parameter]
+        public string Name { get; set; } = string.Empty;
+        [Parameter]
+        public string Image { get; set; } = string.Empty;
+        [Parameter]
+        public decimal Price { get; set; }
+        [Parameter]
+        public decimal PromotionalPrice { get; set; }
+    }
+}
